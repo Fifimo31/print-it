71ceae790596fe156ae6c795ef3774c404a71ee7
Hawa Sall <sallhawa30@yahoo.fr>
2024-06-13 10:29:13 +0200

    correction tagline span

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"tagLine":"Impressions tous formats <span>boutique et en ligne</span>"
 	},
 	{
 		"image":"slide2.jpg",
